ef5adb6f3c9e43636dcc71cd41161f91bae6c010
Victor saad <victorsaad00@gmail.com>
2022-04-25 21:17:12 -0300

    appbar component and register page (part 1)

--- App.js
+++ App.js
@@ -2,77 +2,61 @@ import "react-native-gesture-handler";
 
 import TravelersTheme from "./Themes";
 
-import { Provider as PaperProvider, Divider } from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
 
 import { useLoadedAssets } from "./hooks/useLoadedAssets";
-import { View } from "react-native";
-import Button from "./Themes/Components/Button/Button";
-import Input from "./Themes/Components/Input/Input";
-import Icon from "./images/icon.svg";
-import TravelersIcon from "./assets/Icons/TravelersIcon";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   const isLoadingComplete = useLoadedAssets();
-
-  const card_1 = {
-    world: "Mundo l",
-    name_world: "Ilha Lovelace",
-    progress: [5, 10],
-    description:
-      "Continue junto com Fisher para ajudá-lo a criar o dispositivo Defender.",
-    status: true,
-  };
-
-  const card_2 = {
-    world: "Mundo ll",
-    name_world: "Penhasco de Turing",
-    status: false,
-  };
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <PaperProvider theme={TravelersTheme}>
-        
-        {/* <View
-          style={{
-            height: "100%",
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <TravelersIcon />
-          <Input label="ARROZ" variant="outlined" size="small" />
-          <Divider style={{ height: 25 }} />
-
-          <Input label="ARROZ" variant="outlined" />
-          <Divider style={{ height: 25 }} />
-
-          <Button
-            mode="primary"
-            onClick={() => {
-              console.log("Primary");
-            }}
-          >
-            ENTRAR
-          </Button>
-          <Divider style={{ height: 25 }} />
-          <Button
-            mode="flat"
-            size="small"
-            onClick={() => {
-              console.log("Second");
-            }}
-          >
-            Entrar sem logar
-          </Button>
-        </View> */}
-        <LoginPage/>
+        {/* <LoginPage /> */}
+        <RegisterPage />
       </PaperProvider>
     );
   }
+}
+
+{
+  /* <View
+  style={{
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }}
+>
+  <TravelersIcon />
+  <Input label="ARROZ" variant="outlined" size="small" />
+  <Divider style={{ height: 25 }} />
+
+  <Input label="ARROZ" variant="outlined" />
+  <Divider style={{ height: 25 }} />
+
+  <Button
+    mode="primary"
+    onClick={() => {
+      console.log("Primary");
+    }}
+  >
+    ENTRAR
+  </Button>
+  <Divider style={{ height: 25 }} />
+  <Button
+    mode="flat"
+    size="small"
+    onClick={() => {
+      console.log("Second");
+    }}
+  >
+    Entrar sem logar
+  </Button>
+</View> */
 }
